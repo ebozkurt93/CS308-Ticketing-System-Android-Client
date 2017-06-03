2748341d0e6639cf36bd3b3a66323f2a5444ec65
Erdem Bozkurt <ebozkurt@sabanciuniv.edu>
2017-06-03 23:49:16 +0300

    -

--- app/src/main/java/android/ebozkurt/com/cs308ticket/adapter/EventAdapter.java
+++ app/src/main/java/android/ebozkurt/com/cs308ticket/adapter/EventAdapter.java
@@ -5,6 +5,7 @@ import android.ebozkurt.com.cs308ticket.R;
 import android.ebozkurt.com.cs308ticket.domain.Category;
 import android.ebozkurt.com.cs308ticket.domain.Event;
 import android.support.v7.widget.RecyclerView;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -40,6 +41,13 @@ public class EventAdapter extends RecyclerView.Adapter<EventAdapter.ViewHolder> 
             actor = (TextView) itemView.findViewById(R.id.event_card_actor);
 
             image = (ImageView) itemView.findViewById(R.id.event_card_imageview);
+            
+            itemView.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View v) {
+                    //todo
+                }
+            });
         }
     }
 
